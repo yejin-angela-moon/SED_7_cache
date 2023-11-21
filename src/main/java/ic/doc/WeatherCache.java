fc1e6106fd173c418ff374405b74230c1bf00b22
Yejin Moon <ym1721@pixel38.doc.ic.ac.uk>
2023-11-21 16:16:43 +0000

    WeatherCache class to store forecastwithtime data

--- src/main/java/ic/doc/WeatherCache.java
+++ src/main/java/ic/doc/WeatherCache.java
@@ -1,8 +1,50 @@
 package ic.doc;
 
+import com.weather.Day;
+import com.weather.Forecast;
 import com.weather.Forecaster;
+import com.weather.Region;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class WeatherCache {
     private Forecaster forecaster;
+    private Map<Region, Map<Day, ForecastWithTime>> weatherMap;
+    private final int size;
+    private final long HOUR = TimeUnit.HOURS.toMillis(1);
+
+    public WeatherCache(Forecaster forecaster, int size) {
+        this.forecaster = forecaster;
+        this.weatherMap = new HashMap<>();
+        this.size = size;
+    }
+
+    public Forecast forecastFor(Region region, Day day) {
+        long currentTime = System.currentTimeMillis();
+        // Look into cache first for the forecast
+        if (weatherMap.containsKey(region) && weatherMap.get(region).containsKey(day)) {
+            ForecastWithTime forecastWithTime = weatherMap.get(region).get(day);
+            // if time elapsed is less than an hour
+            if (currentTime - forecastWithTime.getTimestamp() < HOUR) {
+                return forecastWithTime.getForecast();
+            } else {
+//                Refresh hashmap if 1 hour has passed
+//                weatherMap.remove(region);
+            }
+        } else {
+            // if cache doesn't contain the forecast look into the third party client
+            if (weatherMap.size() >= size) {
+                // if maximum size is reached, evict the old entry
+                weatherMap.remove(weatherMap.keySet().iterator().next());
+            }
+            // put a new entry into the cache map
+            Forecast newForecast = forecaster.forecastFor(region, day);
+            weatherMap.computeIfAbsent(region, k -> new HashMap<>()).put(day, new ForecastWithTime(newForecast));
+            return newForecast;
+        }
+    }
+
 
 }
